bc486ef244c88ef730b27294500821f5e98ea765
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2020-10-02 15:12:59 +0500

    create module user-transport-models

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "kotlin-User"
 include("user-mp-transport-models")
 
 pluginManagement{
-    plugins{
+    plugins {
         val kotlinVersion: String by settings
 
         kotlin("multiplatform") version kotlinVersion apply false
@@ -11,3 +11,8 @@ pluginManagement{
     }
 }
 include("user-be-common")
+include("user-mp-transport-models")
+include("user-mp-transport-models")
+include("user-mp-transport-models")
+include("user-mp-transport-models")
+include("user-transport-models")
