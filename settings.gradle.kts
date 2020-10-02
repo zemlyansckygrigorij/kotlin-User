39d9035fa9d5191d441cd54d698608aab5403809
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2020-10-02 12:38:41 +0500

    initial setting file settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
 rootProject.name = "kotlin-User"
+include("user-be-common")
+include("user-mp-transport-models")
+
+pluginManagement{
+    plugins{
+        val kotlinVersion: String by settings
+
+        kotlin("multiplatform") version kotlinVersion apply false
+        kotlin("jvm") version kotlinVersion apply false
+    }
+}
 
